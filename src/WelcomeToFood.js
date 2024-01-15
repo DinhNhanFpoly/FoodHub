9601ac2dcfcb0370b9ac99c1e248b020696dfb01
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-01-16 00:57:59 +0700

    Đình Nhân gửi code
    Nhân mới code được màn hình welcome mai nhân code tiếp

---
+++ src/WelcomeToFood.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WelcomeToFood = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default WelcomeToFood
+
+const styles = StyleSheet.create({
+    container:{
+        width : "100%",
+        height : "100%",
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
